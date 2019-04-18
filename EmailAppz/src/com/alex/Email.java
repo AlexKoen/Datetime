9573a8f6b37988f16b594c80d53f16e0b8769097
AlexKoen <47478546+AlexKoen@users.noreply.github.com>
2019-04-18 12:30:12 +0200

    Add files via upload
    IT Support Administrator Specialist charged with the task of creating email accounts for new hires.

---
+++ EmailAppz/src/com/alex/Email.java
@@ -0,0 +1,99 @@
+package com.alex;
+
+import java.util.Scanner;
+
+public class Email {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private String email;
+    private int mailboxCapacity = 500;
+    private int defaultPasswordLength = 12;
+    private String alternateEmail;
+    private String companySuffix = "ABC/company.com";
+
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        //System.out.println("com.alex.Email created: " + this.firstName + " " + this.lastName);
+
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Your password is " + this.password);
+
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
+        System.out.println("Your email is: " + email);
+    }
+
+
+    private String setDepartment() {
+        System.out.println("New worker " + firstName + ", Enter your depratment code\n1 for sales\n2 for Development\n3 for Accounting\n0 for Admin\nEnter department code: ");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1) {
+            return "Sales";
+        } else if (depChoice == 2) {
+            return "Dev";
+        } else if (depChoice == 3) {
+            return "Accounting";
+        } else {
+            return "Admin";
+        }
+    }
+
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
+        char[] password = new char[length];
+        for (int i = 0; i < length; i++) {
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
+    }
+
+    public void setMailboxCapacity(int capacity) {
+        this.mailboxCapacity = capacity;
+
+    }
+
+    public void setAlternateEmail(String altEmail) {
+        this.alternateEmail = altEmail;
+
+    }
+
+    public void changePassword(String password) {
+        this.password = password;
+
+    }
+
+    public int getMailboxCapacity() {
+        return mailboxCapacity;
+    }
+
+    public String getAlternateEmail() {
+        return alternateEmail;
+    }
+
+    public String getPassword() {
+        return password;
+
+    }
+
+    public String showInfo(){
+        return "Display Name: " + firstName + " " + lastName +
+                "\nCompany Email: " + email + "\nMailbox Capacity: " + mailboxCapacity + "mb";
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ EmailAppz/src/com/alex/Main.java
@@ -0,0 +1,13 @@
+package com.alex;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Email em1 = new Email("Joe", "Black");
+
+
+        em1.showInfo();
+        System.out.println(em1.showInfo());
+
+    }
+}
